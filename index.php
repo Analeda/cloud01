f56a56b8d4a2d4e57f43e8fa617b4414c40991ec
Analeda Casanova <acasanov@alumnos.ubiobio.cl>
2016-10-25 12:19:04 -0300

    Index 2

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
- echo "hola mundo";
- ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>
+		<?php 
+ 			echo "hola mundo";
+ 		?>
+	</h1>
+	<?php 
+		for($i=2;$i<6;$i++){
+ 			echo "<h3>Como estas?</h3>\n";
+		}
+ 		?>
+</body>
+</html>
